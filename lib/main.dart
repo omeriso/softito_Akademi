c1e68fb76d5fa47175d2160133524cd44605e386
Ömer Faruk İsmailoğlu <76107756+omeriso@users.noreply.github.com>
2023-03-07 01:43:15 +0300

    Delete main.dart

--- lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_design/dropdownButton.dart';
-import 'package:flutter_design/home.dart';
-
-void main() {
-  runApp(const HomePage());
-}
